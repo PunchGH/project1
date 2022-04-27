0d039d4f86342aca5478ad82cd8048b2ae4d3d98
PunchGH <104129450+PunchGH@users.noreply.github.com>
2022-04-27 13:22:49 +0700

    Create Punch_py_1.py

---
+++ Punch_py_1.py
@@ -0,0 +1,7 @@
+#num_1 = int(input("enter first number :  "))
+#num_2 = int(input("second number      : "))
+#print(num_1,"+",num_2,"=",num_1+num_2)
+price = int(input("enter ur price   :  "))
+vat = 7
+result = price + (price * vat / 100)
+print(result)
