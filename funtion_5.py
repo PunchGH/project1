b648ed1f3bb79a9742e2a96467f26fee5a506106
PunchGH <104129450+PunchGH@users.noreply.github.com>
2022-04-29 11:12:03 +0700

    lecture54_Punch_I_py

---
+++ funtion_5.py
@@ -0,0 +1,40 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        return False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    return userSelected
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+if login():
+    showMenu()
+    
+    a = menuSelect()
+    
+    if a == 1:
+        print(vatCalculator(100))
+    elif a == 2:
+        print(priceCalculator())        
+    
+ 
+
+    
