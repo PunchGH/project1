f20558544e8301f59b4f81d25f708078a465a5a2
PunchGH <104129450+PunchGH@users.noreply.github.com>
2022-05-06 11:58:05 +0700

    Update collection_2.py

--- collection_2.py
+++ collection_2.py
@@ -1,20 +1,25 @@
-total = 0
+
+systemMenu = {"ไก่ทอด": 35, "เป็ดทอด": 45, "ปลาทอด": 55, "ผักทอด": 20}
 menuList = []
+
 def showBill():
-    
+    total_price = 0
     print("---- My Food----")
-    for number in range(len(menuList)):
-        print(menuList[number])
-        print("total : ",total)
     
+    for number in range(len(menuList)):
+        print(menuList[number][0], menuList[number][1])
+        
+        total_price += menuList[number][1]
+    
+    print(total_price)    
 
 while True:
     menuName = input("Plese Enter Menu :")
     if(menuName.lower() == "exit"):
         break
     else:
-        menuPrice = input("Price :")
-        total += int(menuPrice)
-        menuList.append([menuName,menuPrice])
+        
+        menuList.append([menuName, systemMenu[menuName]])
+        
 
-showBill()
+showBill()                              
